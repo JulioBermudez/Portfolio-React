c37c0760b52c158f97d80f4fd9ba60e9dbdbc7f9
Julio Bermudez <julioph0n3@gmail.com>
2023-11-13 21:42:57 -0500

    fixed bug importing ContactMe Container inside MainContaner.jsx

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,39 +1,39 @@
 import { useState } from "react";
-import AboutMeContainer from "./UI/AboutMeContainer"
-import PortfolioContainer from "./UI/PortfolioContainer"
+import AboutMeContainer from "./UI/AboutMeContainer";
+import PortfolioContainer from "./UI/PortfolioContainer";
 import ContactMeContainer from "./UI/contactMeContainer";
-import HeroContainer from "./UI/HeroContainer"
-import MySkills from "./UI/MySkillsContainer"
+import HeroContainer from "./UI/HeroContainer";
+import MySkills from "./UI/MySkillsContainer";
 import NavBar from "./NavBar/NavBar";
-import Footer from "./UI/Footer/Footer"
+import Footer from "./UI/Footer/Footer";
 
-export default function MainContainer(){
-    const [currentPage, setCurrentPage] = useState('Home');
+export default function MainContainer() {
+  const [currentPage, setCurrentPage] = useState("Home");
 
-    const renderPage = ()=>{
-        if (currentPage === "Home") {
-            return <HeroContainer/>
-        }
-        if (currentPage === "About") {
-            return <AboutMeContainer/>
-        }
-        if (currentPage === "Portfolio") {
-            return <PortfolioContainer/>
-        }
-        if (currentPage === "Contact") {
-            return <ContactMeContainer/>
-        }
-        if (currentPage === "MySkills") {
-            return <MySkills/>
-        }
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <HeroContainer />;
     }
-    const handlePageChange = (page) => setCurrentPage(page)
+    if (currentPage === "About") {
+      return <AboutMeContainer />;
+    }
+    if (currentPage === "Portfolio") {
+      return <PortfolioContainer />;
+    }
+    if (currentPage === "Contact") {
+      return <ContactMeContainer />;
+    }
+    if (currentPage === "MySkills") {
+      return <MySkills />;
+    }
+  };
+  const handlePageChange = (page) => setCurrentPage(page);
 
-    return(
-        <>
-        <NavBar currentPage = {currentPage} handlePageChange={handlePageChange}/>
-        <main>{renderPage()}</main>
-        <Footer/>
-        </>
-    )
+  return (
+    <>
+      <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+      <main>{renderPage()}</main>
+      <Footer />
+    </>
+  );
 }
